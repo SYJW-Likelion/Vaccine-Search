ac1d28bb675a3af6244d8ad14a2008fbcfd689e1
kimminju99 <mindori1019@gmail.com>
2021-09-25 14:25:26 +0900

    index(홈화면), login, signup 화면 구현

--- vaccine/home/views.py
+++ vaccine/home/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from home.api import *
 
 # Create your views here.
+# def home(request):
+#     return render(request, 'home.html')
+
+
 def home(request):
     return render(request, 'index.html')
 
